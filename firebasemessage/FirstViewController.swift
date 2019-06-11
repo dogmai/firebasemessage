16c5b957889b0764f2a3ea18014e6de628d06d86
Matthew Hayward <matthewlvh1@me.com>
2019-06-10 18:01:11 -0700

    Initial Commit

---
+++ firebasemessage/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  firebasemessage
+//
+//  Created by Matthew Hayward on 6/10/19.
+//  Copyright © 2019 2ndLawApps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ firebasemessage/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  firebasemessage
+//
+//  Created by Matthew Hayward on 6/10/19.
+//  Copyright © 2019 2ndLawApps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
